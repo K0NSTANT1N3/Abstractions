05fe683fbe765bb0d103e6334631443825436c4d
K0NSTANT1N3 <programming255.konst.end.01@gmail.com>
2024-06-15 17:18:50 +0400

    exams

--- src/exams/mid-exams/2/unnamed/SumOfTwoLinkedList.cpp
+++ src/exams/mid-exams/2/unnamed/SumOfTwoLinkedList.cpp
@@ -5,6 +5,7 @@
 #include "SumOfTwoLinkedList.h"
 
 
+
 void SumOfTwoLinkedList::reverseHelper(SumOfTwoLinkedList::Node *root, SumOfTwoLinkedList::Node *tail,
                                        SumOfTwoLinkedList::Node *&ans) {
     if (tail == nullptr) {
@@ -27,5 +28,39 @@ SumOfTwoLinkedList::Node *SumOfTwoLinkedList::reverseList(SumOfTwoLinkedList::No
     Node *ans;
     reverseHelper(root, root->next, ans);
     return ans;
+}
+
+
+SumOfTwoLinkedList::Node *
+SumOfTwoLinkedList::sumOfTwoList(SumOfTwoLinkedList::Node *one, SumOfTwoLinkedList::Node *two) {
+    Node *first = reverseList(one);
+    Node *second = reverseList(two);
+
+    Node *head = new Node;
+    Node *tail = head;
+    int carry = 0;
+
+    while (first != nullptr || second != nullptr || carry > 0) {
+        int sum = carry;
+        if (first != nullptr) {
+            sum += first->val;
+            first = first->next;
+        }
+        if (second != nullptr) {
+            sum += second->val;
+            second = second->next;
+        }
+
+        carry = sum / 10;
+        int newVal = sum % 10;
+        tail->next = new Node(newVal, nullptr);
+        tail = tail->next;
+    }
+    Node* realHead = head->next;
+    delete head;
+
+    tail = reverseList(realHead);
+
+    return tail;
 }
 
--- src/exams/mid-exams/2/unnamed/main.cpp
+++ src/exams/mid-exams/2/unnamed/main.cpp
@@ -11,26 +11,36 @@ void printList(SumOfTwoLinkedList::Node* node) {
 }
 
 int main() {
-    // Creating a linked list: 1 -> 2 -> 3 -> 4 -> 5
-    SumOfTwoLinkedList::Node* node5 = new SumOfTwoLinkedList::Node(5, nullptr);
-    SumOfTwoLinkedList::Node* node4 = new SumOfTwoLinkedList::Node(4, node5);
-    SumOfTwoLinkedList::Node* node3 = new SumOfTwoLinkedList::Node(3, node4);
-    SumOfTwoLinkedList::Node* node2 = new SumOfTwoLinkedList::Node(2, node3);
-    SumOfTwoLinkedList::Node* node1 = new SumOfTwoLinkedList::Node(1, node2);
+    // Creating first linked list: 3 -> 4 -> 2 (represents number 243)
+    SumOfTwoLinkedList::Node* firstNode6 = new SumOfTwoLinkedList::Node(3, nullptr);
+    SumOfTwoLinkedList::Node* firstNode5 = new SumOfTwoLinkedList::Node(2, firstNode6);
+    SumOfTwoLinkedList::Node* firstNode4 = new SumOfTwoLinkedList::Node(1, firstNode5);
+    SumOfTwoLinkedList::Node* firstNode3 = new SumOfTwoLinkedList::Node(9, firstNode4);
+    SumOfTwoLinkedList::Node* firstNode2 = new SumOfTwoLinkedList::Node(9, firstNode3);
+    SumOfTwoLinkedList::Node* firstNode1 = new SumOfTwoLinkedList::Node(9, firstNode2);
 
-    std::cout << "Original list: ";
-    printList(node1);
+    // Creating second linked list: 9870
+    SumOfTwoLinkedList::Node* secondNode4 = new SumOfTwoLinkedList::Node(0, nullptr);
+    SumOfTwoLinkedList::Node* secondNode3 = new SumOfTwoLinkedList::Node(7, secondNode4);
+    SumOfTwoLinkedList::Node* secondNode2 = new SumOfTwoLinkedList::Node(8, secondNode3);
+    SumOfTwoLinkedList::Node* secondNode1 = new SumOfTwoLinkedList::Node(9, secondNode2);
+
+    std::cout << "First list: ";
+    printList(firstNode1);
+
+    std::cout << "Second list: ";
+    printList(secondNode1);
 
     SumOfTwoLinkedList solver;
-    SumOfTwoLinkedList::Node* reversedList = solver.reverseList(node1);
+    SumOfTwoLinkedList::Node* sumList = solver.sumOfTwoList(firstNode1, secondNode1);
 
-    std::cout << "Reversed list: ";
-    printList(reversedList);
+    std::cout << "Summed list: ";
+    printList(sumList);
 
     // Free allocated memory
-    while (reversedList != nullptr) {
-        SumOfTwoLinkedList::Node* temp = reversedList;
-        reversedList = reversedList->next;
+    while (sumList != nullptr) {
+        SumOfTwoLinkedList::Node* temp = sumList;
+        sumList = sumList->next;
         delete temp;
     }
 
